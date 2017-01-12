c7ea207f03c4c39b3158008e752268e8a4a3dc84
Patrick Witbrod <pkwitbrod@gmail.com>
2017-01-12 12:57:11 -0600

    Used Yeomen to create the iniatl scaffolding for web part

---
+++ src/webparts/quickLinks/IQuickLinksWebPartProps.ts
@@ -0,0 +1,3 @@
+export interface IQuickLinksWebPartProps {
+  description: string;
+}
---
+++ src/webparts/quickLinks/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IQuickLinksStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'quickLinksStrings' {
+  const strings: IQuickLinksStrings;
+  export = strings;
+}
